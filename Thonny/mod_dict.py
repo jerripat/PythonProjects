4bc2eb810d8ec35770860307370b80dc9afa06fd
jerripat <jerripatterson@gmail.com>
2023-01-22 17:03:30 -0600

    Init commit fr conversion.py

---
+++ Thonny/mod_dict.py
@@ -0,0 +1,8 @@
+d = {'mom':1,'dad':2,'son':3}
+
+def modify_dict(d):
+    d = {'grandma':4,'grandpa':5}
+    return d
+
+modify_dict(d)
+print(d)
---
+++ conversion.py/menu.py
@@ -0,0 +1,16 @@
+import pyMenu
+
+ans = input('Start conversions?(y/n) :')
+
+if ans == 'y':
+        print('Opening menu...')
+        pyMenu.py
+else:
+    print('Closing menu...')
+    exit()
+
+
+
+#def create_menu(ans):
+
+#def menuItems():
