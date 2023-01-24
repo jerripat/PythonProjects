93abcbf597e8513ca670a7ee0f3cf5aae60d10a2
jerripat <jerripatterson@gmail.com>
2023-01-24 10:03:57 -0600

    Update to FreeCodeCamp

---
+++ FreeCodeCamp/Algebra_Python/proportions.py
@@ -0,0 +1,14 @@
+# Put a zero in for the unknown value
+
+n1 =1
+d1 = 2
+n2 = 0
+d2 = 16
+
+if n2==0:
+    answer = d2 * n1 /d1
+    print(f'n2 = {answer}')
+
+if d2==0:
+    answer = n2 * d1 / n1
+    print(f'd2 = {answer}')
