2d3fe4e74cd961d50eac5c0a6c6ac4aa8bee56af
jerripat <jerripatterson@gmail.com>
2023-01-24 09:14:39 -0600

    update conversions

--- Thonny/mod_dict.py
+++ Thonny/mod_dict.py
@@ -1,4 +1,10 @@
-x=3
-y=4
-z=5
-print(x if not x+y*z else y)
+class Num:
+    def __init__(self,value):
+        self.value = value
+        
+    def __add__(self, other):
+        return self.value - other.value
+    
+x = Num(5)
+y = Num(10)
+print(x+y)
--- conversion.py/formulas.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-def FtoC(x):
-    # F = (9/5 * C) + 32
-
-    f = x
-    return ((f - 32) * (5/9))
