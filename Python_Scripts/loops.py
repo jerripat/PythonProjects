fe177876d394173ce2d8631a99f97055cd0c2e2d
jerripat <jerripatterson@gmail.com>
2025-08-27 01:56:49 -0500

    init commit

---
+++ Python_Scripts/loops.py
@@ -0,0 +1,8 @@
+for each_char in "python Scripting":
+    if each_char.isalpha():
+        print(each_char)
+
+index = 0;
+for each_char in "python":
+    print(f"Character at index {index}: {each_char}")
+    index += 1
